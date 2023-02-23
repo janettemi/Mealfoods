143af03a06a1377cc5d813580c280c20e1d2dd0d
janettemi <ogunnaikejanettemitope@gmail.com>
2023-02-23 12:49:54 +0100

    Food meals

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import CartIcon from "../Cart/Carticon";
+import classes from './HeaderCartButton'
+const HeaderCartButton = props => {
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon></CartIcon>
+        </span>
+        <span> Your Cart</span>
+        <span className={classes.badge}>3</span>
+    </button>
+};
+
+export default HeaderCartButton;
